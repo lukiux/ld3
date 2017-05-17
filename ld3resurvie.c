3dbfbf0962b5d2a65660ac81402606362ee69678
lukiux <lukaukas@gmail.com>
2017-05-18 01:32:48 +0300

    Create ld3resurvie.c

---
+++ ld3resurvie.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <string.h>
+#include <dlfcn.h>
+#include <dirent.h>
+
+void listdir(const char *name, int level)
+{
+        DIR *dir;
+        struct dirent *entry;
+
+        if(!(dir = opendir(name)))
+                return;
+        if(!(entry = readdir(dir)))
+                return;
+        do {
+                        int status;
+                        struct stat buffer;
+                        status = stat(entry->d_name, &buffer);
+                        if(S_ISREG(buffer.st_mode)){
+                         if(buffer.st_size >= 1048576)
+                                        chmod(entry->d_name,S_IRWXU);
+                        }
+                else if (S_ISDIR(buffer.st_mode)){
+                        char path[1024];
+                        int len = snprintf(path, sizeof(path)-1, "%s/%s", name, entry->d_name);
+                        path[len] = 0;
+                        int status;
+                        struct stat buffer;
+                        status = stat(entry->d_name, &buffer);
+                        if(S_ISREG(buffer.st_mode)){
+                                if(buffer.st_size >= 1048576)
+                                        chmod(entry->d_name,S_IRWXU);
+                       }
+                        listdir(path,level+1);
+                        }
+        } while((entry = readdir(dir)) != NULL);
+        closedir(dir);
+}
+int main(int argc, char *argv[]){
+        listdir(".", 0);
+        return 0;
+}
+
